7214e6ad7f0ec674eaa3e697f6f849fe4095d90e
John Nguyen <johnnguyen4501@gmail.com>
2025-06-14 01:57:45 -0400

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "John Nhat Nguyen | Portfolio",
@@ -22,6 +23,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
